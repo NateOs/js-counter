0fb7d152b797a147517aff0ed54a8557970bee41
nateos <sodjanathan@gmail.com>
2021-01-22 23:15:46 +0000

    Done

--- app.js
+++ app.js
@@ -10,7 +10,22 @@ btns.forEach( btn => {
        const styles = e.currentTarget.classList
        if (styles.contains('decrease')) { //contains only applies to DOM nodes, includes for arrays
         count--
+       } else if (styles.contains('increase')) {
+        count++  
+       } else {
+        count = 0
        }
+       
+       //changing colors
+       if (count > 0) {
+           value.style.color = "green"
+       } else
+       if (count < 0) {
+        value.style.color = "red"   
+       } else {
+        value.style.color = "black"  
+       }
+
        value.textContent = count
     })
 })
